bb7c1a59ef8e6ef817433087def7bb1b6dd0b176
Anton Bachin <antonbachin@yahoo.com>
2021-05-17 13:58:42 +0300

    Add /_livereload route in the example (#3)

--- example/server.ml
+++ example/server.ml
@@ -4,5 +4,8 @@ let () =
   Dream.run ~debug:true
   @@ Dream.logger
   @@ Dream_livereload.inject_script ()
-  @@ Dream.router [ Dream.get "/" (fun _ -> Dream.html "Hello World!") ]
+  @@ Dream.router [
+    Dream.get "/" (fun _ -> Dream.html "Hello World!");
+    Dream_livereload.route ();
+  ]
   @@ Dream.not_found
